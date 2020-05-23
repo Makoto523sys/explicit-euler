fe4109afa35708918d6ae38ae070819d6d4157bd
Makoto <ookunimasaharu@gmail.com>
2020-05-24 00:16:57 +0900

    I have to code explicit_euler.h

--- explicit_euler.h
+++ explicit_euler.h
@@ -8,6 +8,22 @@ class explicit_euler: public parameter{
 	private:
 		double* u;
 	public:
-		inline void next(void);
-		inline void;
+		explicit explicit_euler(double _dx, double _dt): parameter{_dx, _dt}{
+			u = new double((int)std::pow(_dt, -1));
+		}
+		explicit explicit_euler(double _dx, double _dt, _kappa): parameter{_dx, _dt, _kappa}{
+			u = new double((int)std::pow(_dt, -1));
+		}
+		explicit explicit_euler(): parameter{0.001, 0.01}{
+			u = new double((int)std::pow(_dt, -1));
+		}
+		~explicit_euler(){
+			delete[] u;
+		}
+		inline void next(void)const;
+		inline void step_to(int)const;
+		inline void solve(void)const;
 };
+
+void explicit_euler::next(void)const{
+
